30337b3e18074130c4fddb869796cfe2c542f014
Mukesh <ganatram@gmail.com>
2026-06-08 15:28:57 +0530

    firstreactapp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import { Alert } from './Alert';
 
 export function App() {
   return (
-    <Alert type="information" heading="success" closable>
+    <Alert type="information" heading="success" closable={false}>
       everything is really good
     </Alert>
   );
