3349f3c85680950c53c3b182da23571c07553eae
Mukesh <ganatram@gmail.com>
2026-06-08 13:05:39 +0530

    firstreactapp

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  // functional component
+export function App() {
+  // ES6 binding
   return <div> placeholder for app component </div>;
 }
-
-export default App;
